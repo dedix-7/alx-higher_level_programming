9d8fd8a6a5e75af199be986f0d4951f2d0173df8
nox <anzimboingosi@gmail.com>
2024-03-12 17:12:11 -0700

    use floyd alg

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked lists has a cycle
+ * @list: head of the lists
+ * Return: 0 if it doesn't 1 if it does
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, fast;
+
+	if (list->next == NULL)
+		return (0);
+	slow = list;
+	fast = list;
+	while (fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
