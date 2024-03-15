c6d8db2bc0fe2e2f1b8fba68e27410eaeba521bc
enoxcodes <anzimboingosi@gmail.com>
2024-03-15 03:01:23 +0300

    use fmod and consteuct int

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,12 @@
 import random
 import math
 number = random.randint(-10000, 10000)
-dig = abs(math.fmod(number, 10))
-print(f"Last digit of {number} is {dig}", end=" ")
+dig = math.fmod(number, 10)
+dig = int(dig)
+print(f"Last digit of {number} is {dig} ", end=" ")
 if (dig > 5):
-    print(f"and is greater than 5")
+    print("and is greater than 5")
 elif (dig == 0):
-    print(f"and is 0")
+    print("and is 0")
 elif ((dig < 6) and (dig != 0)):
-    print(f"and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
