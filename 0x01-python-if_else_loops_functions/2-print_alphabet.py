3bf3f8ddf97d9606fc00230ba836157f6cbbab19
enoxcodes <anzimboingosi@gmail.com>
2024-03-15 03:16:22 +0300

    use for loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # a script to print lowercase chars using ascii
-string = ''.join(chr(i) for i in range(97, 123))
-print(string,end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
