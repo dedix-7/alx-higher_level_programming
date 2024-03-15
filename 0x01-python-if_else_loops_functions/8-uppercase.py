d131ed80f0bd5566cf945d3f780cf36aadc48e88
noxcodes <anzimboingosi@gmail.com>
2024-03-15 10:45:22 -0700

    make newline at the end

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# a module to print a entence in uppercase
+
+def uppercase(str):
+    """ A function to print uppercase repr of strings
+    """
+
+    for c in str:
+        char = ord(c)
+        low = ord(c)
+        upp = ord(c) - 32
+        if low in range(97, 123):
+            char = upp
+        else:
+            char = low
+        print("{}".format(chr(char)), end="")
+    print()
