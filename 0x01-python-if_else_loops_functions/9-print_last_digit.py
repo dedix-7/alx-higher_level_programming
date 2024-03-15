0db043499eb7d170161105416d588b57ed51c93e
noxcodes <anzimboingosi@gmail.com>
2024-03-15 16:10:50 -0700

    write without newline

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,8 +4,8 @@
 def print_last_digit(number):
     if (number < 0):
         num = -1 * number
-        print("{}".format(num % 10))
+        print("{}".format(num % 10), end="")
         return (num % 10)
     else:
-        print("{}".format(number % 10))
+        print("{}".format(number % 10), end="")
         return (number % 10)
