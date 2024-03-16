864d08466aa86e3152ef461599a61d635e6f036a
noxcodes <anzimboingosi@gmail.com>
2024-03-16 13:38:13 -0700

    acc for one args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ length = len(sys.argv)
 if __name__ == "__main__":
     if (length == 1):
         print("0 arguments.")
+    elif (length == 2):
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
     else:
         print(f"{length - 1} arguments:")
         for i, arg in enumerate(sys.argv[1:], start=1):
