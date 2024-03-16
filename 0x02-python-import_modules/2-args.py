08aab988c4f17e595ed16342f5b86cea10bca359
noxcodes <anzimboingosi@gmail.com>
2024-03-16 13:35:28 -0700

    don't execute when importing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,10 @@
 # a module to check commandline arguments
 import sys
 length = len(sys.argv)
-if (length == 1):
-    print("0 arguments.")
-else:
-    print(f"{length - 1} arguments:")
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    if (length == 1):
+        print("0 arguments.")
+    else:
+        print(f"{length - 1} arguments:")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i}: {arg}")
