0ec81bd728cb46b5f960870bab08a0c380d05310
noxcodes <anzimboingosi@gmail.com>
2024-03-16 13:44:39 -0700

    add given args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# a module to add all given arguments
+
+import sys
+lengt = len(sys.argv)
+num = 0
+if (lengt > 1):
+    for i in (sys.argv[1:]):
+        num += int(i)
+print(f"{num}")
