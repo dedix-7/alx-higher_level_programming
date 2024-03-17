5b0174032e66b23a1debed926ef74dd237672e2e
noxcodes <anzimboingosi@gmail.com>
2024-03-17 05:05:21 -0700

    account for none being passed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 # a script to print all elems ofa list in reverse
 
 def print_reversed_list_integer(my_list=[]):
-    for i in (my_list[::-1]):
-        print("{:d}".format(i))
+    if my_list:
+        for i in (my_list[::-1]):
+            print("{:d}".format(i))
