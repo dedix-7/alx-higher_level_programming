788398c477859be04b33adec8868cd153137d915
noxcodes <anzimboingosi@gmail.com>
2024-03-17 00:18:59 -0700

    use cmdline args

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # simple calc with command line args
 
-import sys, calculator_1
+import sys
+import calculator_1
 if __name__ == "__main__":
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -9,8 +10,8 @@ if __name__ == "__main__":
     if (sys.argv[2] not in ['+', '-', '*' , '/']):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if (sys.argv[2] == '+'):
         op = calculator_1.add
     elif (sys.argv[2] == '-'):
