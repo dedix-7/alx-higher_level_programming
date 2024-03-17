fe83a81bc973ae96dad45b63e4a097fca45f5259
noxcodes <anzimboingosi@gmail.com>
2024-03-17 05:11:05 -0700

    create a copy of the list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/in/python3
+# a module to replace an lement in a copy
+
+def new_in_list(my_list, idx, element):
+    if (my_list == None):
+        return None
+    if (idx < 0):
+        return my_list[:]
+    if (idx >= len(my_list)):
+        return my_list[:]
+    new = my_list[:]
+    new[idx] = element
+    return (new)
