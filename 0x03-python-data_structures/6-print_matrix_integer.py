4c8c667477bfcc797d1629ce31e16bc9aec85982
noxcodes <anzimboingosi@gmail.com>
2024-03-17 05:57:36 -0700

    prrint a matrix of matrices

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a module to print a matrix of matrices
+
+def print_matrix_integer(matrix=[[]]):
+    if (matrix):
+        for row in matrix:
+            for col in row:
+                print("{:d}".format(col), end=" ")
+            print
