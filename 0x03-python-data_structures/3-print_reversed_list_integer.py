11826c73436b391d838b47e1e316ab7f3306377b
noxcodes <anzimboingosi@gmail.com>
2024-03-17 05:00:56 -0700

    use reversed method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 # a script to print all elems ofa list in reverse
 
 def print_reversed_list_integer(my_list=[]):
-    for i in (my_list[::-1]):
+    for i in (my_list.reversed()):
         print("{:d}".format(i))
