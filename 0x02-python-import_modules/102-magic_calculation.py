bc764496cdae3ace8add9af03a241fde7a1bb44d
noxcodes <anzimboingosi@gmail.com>
2024-03-17 00:43:23 -0700

    reinterpret python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,5 +5,7 @@ def magic_calculation(a, b):
     from magic_calculation_102 import sub, add
     if (a < b):
         c = add(a, b)
-    for i in range(90):
-        
+        for i in range(4, 6):
+            c = add(i, c)
+        return (c)
+    return (sub(a, b))
