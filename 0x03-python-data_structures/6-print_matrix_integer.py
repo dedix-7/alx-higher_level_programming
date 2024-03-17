0aefe834578aae34f161acdcb4aa11f6e682f768
noxcodes <anzimboingosi@gmail.com>
2024-03-17 06:22:21 -0700

    make newline after rows

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -11,3 +11,4 @@ def print_matrix_integer(matrix=[[]]):
                 else:
                     endchar = ""
                 print("{:d}".format(matrix[i][j]), end=endchar)
+            print()
