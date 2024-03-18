6806cd27431ca7a7fed9c9bcef31d7457c1e0b61
noxcodes <anzimboingosi@gmail.com>
2024-03-18 12:20:07 -0700

    use del to mutatae list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,10 +4,5 @@
 def delete_at(my_list=[], idx=0):
     if ((idx < 0) or (idx >= len(my_list))):
         return (my_list)
-    new = []
-    for i in range(len(my_list)):
-        if (i == idx):
-            continue
-        else:
-            new.append(my_list[i])
-    return (new)
+    del my_list[idx]
+    return (my_list)
