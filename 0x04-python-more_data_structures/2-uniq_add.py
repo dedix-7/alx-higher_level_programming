fe2b2b384a8bb9007b12a1e32002848c4714af2c
noxcodes <anzimboingosi@gmail.com>
2024-03-20 11:35:14 -0700

    add uniqa

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# add all unique elements in a list
+
+def uniq_add(my_list=[]):
+    # I'll use set instaed of making a new list
+    check = set(my_list)
+    psum = 0
+    for i in my_list:
+        if i in check:
+            continue
+        psum += i
+    return (psum)
