a2d5af889eea5bb78ebf958c887fc42abda06be4
noxcodes <anzimboingosi@gmail.com>
2024-03-20 17:40:23 -0700

    update dictionary function

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# update te value of a key in a dict
+
+def update_dictionary(a_dictionary, key, value):
+    # create one if it doesn't exists
+    a_dictionary[key] = value
+    return (a_dictionary)
