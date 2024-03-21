3733a2d0fbe2dccf2ddb5ce8b0df5cc90843151a
noxcodes <anzimboingosi@gmail.com>
2024-03-20 17:34:13 -0700

    style code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     pr = sorted(a_dictionary)
-    ret = {pr[i]:a_dictionary.get(pr[i]) for i in range(len(pr))}
+    ret = {pr[i]: a_dictionary.get(pr[i]) for i in range(len(pr))}
     for k, v in ret.items():
-        print (f"{k}: {v}")
+        print(f"{k}: {v}")
     return (ret)
