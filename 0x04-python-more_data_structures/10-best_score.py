deaced377445d33960e1595fb3285b3b46bc61a3
noxcodes <anzimboingosi@gmail.com>
2024-03-20 18:18:08 -0700

    use inbuilt max func

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# a script to get the biggest score
+
+def best_score(a_dictionary):
+    if (a_dictionary):
+        return (max(zip(a_dictionary.values(), a_dictionary.keys()))[])
