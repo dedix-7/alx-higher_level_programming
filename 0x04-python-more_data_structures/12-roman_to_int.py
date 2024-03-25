3a91c96fee4c28e404d9dbe856cabb8aec741041
enoxcodes <anzimboingosi@gmail.com>
2024-03-25 09:12:09 +0300

    check that arguent is a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 # a script to convert roman numerals to ints
 
 def roman_to_int(roman_string):
-    if (roman_string is None):
+    if ((roman_string is None) or (type(roman_string) is not str)):
         return (0)
     res = 0
-    check = {'I':1, 'V':5,'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
+    check = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for c in (range(len(roman_string))):
         curr = check.get(roman_string[c])
         if (c):
