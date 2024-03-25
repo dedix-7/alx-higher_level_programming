741e15ba5d41b3217f733ce2b68e3ddb1cc71841
enoxcodes <anzimboingosi@gmail.com>
2024-03-25 09:06:05 +0300

    make a script to convert roman numerals to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,20 @@
 # a script to convert roman numerals to ints
 
 def roman_to_int(roman_string):
-    if (!roman_string):
+    if (roman_string is None):
         return (0)
     res = 0
-    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    check = {'I':1, 'V':5,'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
     for c in (range(len(roman_string))):
-        val = roman_string[c]
-        val1 = 0
-        if c >= len(roman_string):
-            val1 = roman_string[c + 1]
-        if (val1):
-            if (val1 > val)
+        curr = check.get(roman_string[c])
+        if (c):
+            prev = check.get(roman_string[c - 1])
+            if (curr > prev):
+                res -= prev
+                res += (curr - prev)
+                continue
+            else:
+                res += curr
+                continue
+        res += curr
+    return (res)
