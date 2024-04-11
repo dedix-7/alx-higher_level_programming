9bdb69565a3f743dbcf9253a458943a9a5c7c891
enoxcodes <anzimboingosi@gmail.com>
2024-04-11 17:04:17 +0300

    advn

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(given=[]):
+    given.append('BestSchool')
+    return (', '.join(given))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This is a class limiting objects created with it
+"""
+
+
+class LockedClass():
+    """ a class with limited instance objects
+    """
+
+    __slots__("first_name")
