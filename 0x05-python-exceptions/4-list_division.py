b14ffc7485a32c5ab8b0c5dd6f9acf1c258a00ec
enoxcodes <anzimboingosi@gmail.com>
2024-04-14 21:38:20 +0300

    ensure 0 for no divs

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,6 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             elem = my_list_1[i] / my_list_2[i]
         except (TypeError):
             print('wrong type')
+            elem = 0
         except (ZeroDivisionError):
             print('division by 0')
             elem = 0
