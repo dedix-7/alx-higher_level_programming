899118ff5f82b5a5dc5ba647e950fa02d0d48dfc
enoxcodes <anzimboingosi@gmail.com>
2024-04-14 22:20:21 +0300

    vhxsag

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     # a funtion to return others
     try:
-        res = fct(args)
+        res = fct(*args)
     except Exception as mess:
         print("Exception: {}".format(mess), file=sys.stderr)
         return
