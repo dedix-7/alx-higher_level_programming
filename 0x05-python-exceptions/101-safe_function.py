bd1d177add28f264679eea227ae7a068749a0875
enoxcodes <anzimboingosi@gmail.com>
2024-04-14 22:16:11 +0300

    use first clas fns

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    # a funtion to return others
+    try:
+        res = fct(args)
+    except Exception as mess:
+        print("Exception: {}".format(mess), file=sys.stderr)
+        return
+    return res
