2d56f5f6d4c2c552ea1d16a6dca0496daa75e477
enoxcodes <anzimboingosi@gmail.com>
2024-04-14 21:58:44 +0300

    use file to com

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+# print an int and errs to stderr
+
+def safe_print_integer_err(value):
+    # funtion to print the int
+    try:
+        print("{:d}".format(value))
+    except Exception as mess:
+        print("Exception: {}".format(mess), file=sys.stderr)
+        return (False)
+    return (True)
