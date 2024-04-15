569a0ea1494f43cd247609383006ea26c7f35181
enoxcodes <anzimboingosi@gmail.com>
2024-04-15 10:14:19 +0300

    rever bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# make reverseed fuinction
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        except:
+            if (i > a):
+                raise Exception('Too far')
+            result = ((a ** b) / i)
+
+        result = a + b
+    return (result)
