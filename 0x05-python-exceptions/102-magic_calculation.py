75d64510ed7d6428e6c5435e0283f37701ef42ce
enoxcodes <anzimboingosi@gmail.com>
2024-04-15 11:32:52 +0300

    byterev

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,10 +3,7 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        except:
+        try:
             if (i > a):
                 raise Exception('Too far')
-            result = ((a ** b) / i)
-
-        result = a + b
-    return (result)
+        result += ((a ** b) / i)
