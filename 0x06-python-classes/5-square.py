10e30cd2929d694345514d4513bbaf6b045ca497
noxcodes <anzimboingosi@gmail.com>
2024-04-21 13:29:24 -0700

    check for 0

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -48,6 +48,9 @@ class Square:
               self - instance
         """
 
+        if (self.__size == 0):
+            print()
+            return
         for i in range(self.__size):
             for j in range(self.__size):
                 print('#', end='')
