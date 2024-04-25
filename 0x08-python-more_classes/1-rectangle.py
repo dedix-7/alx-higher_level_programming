dcfd21084044079dc6a788475e1f03b406ef960e
noxcodes <anzimboingosi@gmail.com>
2024-04-25 01:29:41 -0700

    make proper commit messages

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -46,9 +46,9 @@ class Rectangle:
         """
 
         if (type(value) is not int):
-            raise TypeError('width must be an integer')
+            raise TypeError('height must be an integer')
         if (value < 0):
-            raise ValueError('width must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
         return (self.__height)
 
@@ -62,12 +62,12 @@ class Rectangle:
 
         if (type(width) is not int):
             raise TypeError('width must be an integer')
-        if (height < 0):
+        if (width < 0):
             raise ValueError('width must be >= 0')
         self.__width = width
 
         if (type(height) is not int):
-            raise TypeError('width must be an integer')
+            raise TypeError('height must be an integer')
         if (height < 0):
-            raise ValueError('width must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = height
