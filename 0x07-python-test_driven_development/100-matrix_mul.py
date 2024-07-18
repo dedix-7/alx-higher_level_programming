237fac4f3ac727acd126ee361bf148cc73fc7892
noxcodes <anzimboingosi@gmail.com>
2024-07-18 09:40:36 -0700

    return the matrix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -22,6 +22,34 @@ def matrix_mul(m_a, m_b):
         raise TypeError('m_b must be a list of lists')
     if (len(m_a) == 0):
         raise ValueError("m_a can't be empty")
+    if (len(m_a) == 1):
+        if (len(m_a[0]) == 0):
+            raise ValueError("m_a can't be empty")
     for i in m_a:
-        if not (all(isinstance(c, int) for c in m_a)):
-            raise 
+        if (len(i) != len(m_a[0])):
+            raise TypeError('each row of m_a must be of the same size')
+        if not (all(isinstance(d, (int, float)) for d in i)):
+            raise TypeError("m_a should contain only integers or floats")
+    if (len(m_b) == 0):
+        raise ValueError("m_b can't be empty")
+    if (len(m_b) == 1):
+        if (len(m_b[0]) == 0):
+            raise ValueError("m_b can't be empty")
+    for i in m_b:
+        if (len(i) != len(m_b[0])):
+            raise TypeError('each row of m_b must be of the same size')
+        if not (all(isinstance(d, (int, float)) for d in i)):
+            raise TypeError("m_b should contain only integers or floats")
+    if (len(m_a[0]) != (len(m_b))):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = []
+    for i in range(len(m_a)):
+        res = []
+        for j in range(len(m_b[0])):
+            star = 0
+            for k in range(len(m_b)):
+                star += m_a[i][k] * m_b[k][j]
+            res.append(star)
+        result.append(res)
+    return (result)
