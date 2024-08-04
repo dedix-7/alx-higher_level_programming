b4604dadce02dfe08fe6d8762cba710bba96e906
noxcodes <anzimboingosi@gmail.com>
2024-08-04 15:52:24 +0300

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,18 +40,35 @@ class Square(rectangle.Rectangle):
         """ method to update the attributes
         """
 
-        attributes = [self.id, self.__size, self.__x, self.__y]
-        guesses = [id, width, height, x, y]
-        for i in range(len(args)):
-            x = attributes[i]
-            x = args[i]
-        if ((len(args) <= 0)):
-            if ((len(kwargs) <= 0)):
-                pass
-            else:
-                for key, value in kwargs:
-                    if (key in guesses):
-                        setattr(self, key, value)
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if kwargs is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
         """ a function to return the dictionary representation of a rectangle
