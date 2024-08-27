670bdcece39d12463d6b5907dcd4c00919caa004
noxcodes <anzimboingosi@gmail.com>
2024-08-27 20:36:48 +0300

    use the repeat method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,9 +9,7 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        console.log('X');
-      }
+        console.log('X'.repeat(this.width));
     }
   }
 };
